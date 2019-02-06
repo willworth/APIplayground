be795bc7d710615b8d722bd85b2001528c89e78c
Will Worth <will.worth@gmail.com>
2019-02-06 16:26:14 +0100

    styles working

--- index.js
+++ index.js
@@ -1,44 +1,28 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
-
-
-const app = express();
-
-app.use(bodyParser.urlencoded({extended: true}));
-
-
-app.get("/", function(req, res){
-    res.sendFile(__dirname + "/index.html");
-})
-
-
-app.listen(3000, function(){
-    console.log('app running on port 3000!');
-    
-});
-const express = require("express");
-const bodyParser = require("body-parser");
-
-
+const path = require("path");
 
 const app = express();
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
-app.use(bodyParser.urlencoded({extended: true}));
 
-
-app.get("/", function(req, res){
-    res.sendFile(__dirname + "/index.html");
-})
+app.use(express.static(__dirname + '/public'));
+///////////ROUTES/////////
 
 app.post("/", function(req, res){
-
     console.log(req.body)
-
 });
+
+
+
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+})
 
 
 app.listen(3000, function(){
     console.log('app running on port 3000!');
     
 });
+
