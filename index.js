b9498ef0a121c2fb975ac41c9d59b17f30131ad6
Will Worth <will.worth@gmail.com>
2019-02-06 20:51:00 +0100

    rendering

--- index.js
+++ index.js
@@ -13,9 +13,12 @@ app.post("/", function(req, res){
     // console.log(req.body.crypto); 
     // request("url", function(error, response, body){
     request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD", function(error, response, body){
-        console.log(response.statusCode);
-        console.log(body);
-
+        // console.log(response.statusCode);
+        // console.log(body);
+        let data = JSON.parse(body);
+        let price = data.last;
+        console.log(price);
+        res.send("<h1>The current price of Bitcoin is " + price+ "USD</h1>")
     })
 });
 
