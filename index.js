d0a84bc71fab8f6ff2c1dac5075cad6bc23dfd2e
Will Worth <will.worth@gmail.com>
2019-02-06 21:14:11 +0100

    res write example and date added

--- index.js
+++ index.js
@@ -10,15 +10,28 @@ app.use(express.static(__dirname + '/public'));
 ///////////ROUTES/////////
 
 app.post("/", function(req, res){
-    // console.log(req.body.crypto); 
-    // request("url", function(error, response, body){
-    request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD", function(error, response, body){
+        // console.log(req.body.crypto); 
         // console.log(response.statusCode);
-        // console.log(body);
-        let data = JSON.parse(body);
-        let price = data.last;
-        console.log(price);
-        res.send("<h1>The current price of Bitcoin is " + price+ "USD</h1>")
+        let crypto = req.body.crypto;
+        let fiat = req.body.fiat;
+        // console.log("the crypto is " + crypto);
+        // console.log("the fiat is " +fiat);
+        
+        const baseUrl = "https://apiv2.bitcoinaverage.com/indices/global/ticker/";
+        const finalUrl = baseUrl + crypto + fiat;
+        
+        request(finalUrl, function(error, response, body){
+            let data = JSON.parse(body);    
+            let price = data.last;
+            let currentDate = data.display_timestamp;
+            //can't do multiple res.send so use res.write for temp storage...
+            res.write("<p>The current date is " + currentDate + "</p>");
+            res.write("<h1>The current price of " + crypto + " is: " + price + " in " + fiat +  "</h1>");
+
+
+            console.log(price);
+        // res.send("<h1>The current price of " + crypto + " is: " + price + " in " + fiat +  "</h1>")
+        res.send();
     })
 });
 
